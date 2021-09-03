8d2a75669d4bc38f0a64b276eb90d993b49f54f7
Xingyu Liu <xliu315@aucklanduni.ac.nz>
2021-09-03 20:46:44 +1200

    task10 backup

--- people_web_app/__init__.py
+++ people_web_app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask, request, url_for
-
 import people_web_app.adapters.repository as repo
 from people_web_app.domain.model import Person
 
-
 def create_app():
     app = Flask(__name__)
-
+    app.config.from_object('config.Config')
     repo.repo_instance = repo.PeopleRepository(
         Person(74633, 'Julius', 'Caeser'),
         Person(88337, 'Genghis', 'Khan'),
@@ -14,5 +12,7 @@ def create_app():
         Person(12826, 'Mahatma', 'Ghandi'),
         Person(92213, 'Nelson', 'Mandela')
     )
-
+    with app.app_context():
+        from.people_blueprint import people
+        app.register_blueprint(people.people_blueprint)
     return app
